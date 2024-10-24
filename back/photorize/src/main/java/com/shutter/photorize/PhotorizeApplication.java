5e9f3504df8af0a0a0a9aedf0c1b87fe5f57b930
Lee9Bin <imagen33@daum.net>
2024-10-24 14:47:21 +0900

    Init: 프로젝트 초기 환경 설정
    initialize
    프로젝트 초기 설정
    
    S11P31A207-53

---
+++ back/photorize/src/main/java/com/shutter/photorize/PhotorizeApplication.java
@@ -0,0 +1,13 @@
+package com.shutter.photorize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotorizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotorizeApplication.class, args);
+	}
+
+}
---
+++ back/photorize/src/test/java/com/shutter/photorize/PhotorizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shutter.photorize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotorizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
