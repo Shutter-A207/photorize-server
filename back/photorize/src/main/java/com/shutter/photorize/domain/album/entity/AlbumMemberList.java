591798e64a142d2fe0fa75cf127a5b47c24cffe4
Lee9Bin <imagen33@daum.net>
2024-10-28 17:46:00 +0900

    Feat: AlbumMemberList 엔티티 생성
    AlbumMemberList
    AlbumMemberList 엔티티 생성
    
    S11P31A207-147

---
+++ back/photorize/src/main/java/com/shutter/photorize/domain/album/entity/AlbumMemberList.java
@@ -0,0 +1,36 @@
+package com.shutter.photorize.domain.album.entity;
+
+import com.shutter.photorize.domain.member.entity.Member;
+import com.shutter.photorize.global.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AlbumMemberList extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "album_id", nullable = false)
+	private Album album;
+
+	@ManyToOne
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	@Column(nullable = false)
+	private boolean status;
+}
