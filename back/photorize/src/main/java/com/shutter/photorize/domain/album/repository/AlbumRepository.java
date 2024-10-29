eb2c847b9bd21991d13cbf76957d51c57c1ca260
Lee9Bin <imagen33@daum.net>
2024-10-29 14:57:22 +0900

    Fix: type 수정
    AlbumRepository
    잘못된 type을 Album으로 수정
    
    S11P31A207-157

--- back/photorize/src/main/java/com/shutter/photorize/domain/album/repository/AlbumRepository.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/album/repository/AlbumRepository.java
@@ -3,6 +3,8 @@ package com.shutter.photorize.domain.album.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.shutter.photorize.domain.album.entity.Album;
+
 @Repository
-public interface AlbumRepository extends JpaRepository<AlbumRepository, Long> {
+public interface AlbumRepository extends JpaRepository<Album, Long> {
 }
