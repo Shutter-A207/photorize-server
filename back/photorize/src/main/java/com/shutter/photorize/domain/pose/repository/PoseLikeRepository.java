14f71d9c6315f2b25663c7e9b35004f26948c1a4
박진형 <dyunames21@gmail.com>
2024-10-29 14:45:04 +0900

    Feat: 포즈 관련 API 레포 형식 수정

--- back/photorize/src/main/java/com/shutter/photorize/domain/pose/repository/PoseLikeRepository.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/pose/repository/PoseLikeRepository.java
@@ -3,11 +3,13 @@ package com.shutter.photorize.domain.pose.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.shutter.photorize.domain.member.entity.Member;
 import com.shutter.photorize.domain.pose.entity.Pose;
 import com.shutter.photorize.domain.pose.entity.PoseLike;
 
+@Repository
 public interface PoseLikeRepository extends JpaRepository<PoseLike, Long> {
 	boolean existsByMemberAndPose(Member member, Pose pose);
 
--- back/photorize/src/main/java/com/shutter/photorize/domain/pose/repository/PoseRepository.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/pose/repository/PoseRepository.java
@@ -1,8 +1,10 @@
 package com.shutter.photorize.domain.pose.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.shutter.photorize.domain.pose.entity.Pose;
 
+@Repository
 public interface PoseRepository extends JpaRepository<Pose, Long> {
 }
