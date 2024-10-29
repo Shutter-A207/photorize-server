0a335274b6655bacb0baa93aeac419403417a4ce
Lee9Bin <imagen33@daum.net>
2024-10-29 17:50:23 +0900

    Feat: 서버 에러 핸들러 메서드 구현
    GlobalExceptionHandler
    서버 에러 핸들러 메서드 구현
    
    S11P31A207-191

--- back/photorize/src/main/java/com/shutter/photorize/global/exception/GlobalExceptionHandler.java
+++ back/photorize/src/main/java/com/shutter/photorize/global/exception/GlobalExceptionHandler.java
@@ -7,12 +7,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.shutter.photorize.global.error.ErrorType;
 import com.shutter.photorize.global.response.ApiResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(PhotorizeException.class)
 	public ResponseEntity<ApiResponse<Void>> handlePhotorizeException(PhotorizeException ex) {
 		ErrorType errorType = ex.getErrorType();
-		return ApiResponse.error(errorType.getStatus(), errorType.getMessage());
+		log.error("PhotorizeException: {}", errorType.getMessage(), ex);
+		return ApiResponse.error(errorType, errorType.getMessage());
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ApiResponse<Void>> handleGeneralException(Exception ex) {
+		log.error("Exception: {}", ex.getMessage(), ex);
+		return ApiResponse.error(ErrorType.DEFAULT_ERROR, ErrorType.DEFAULT_ERROR.getMessage());
 	}
 }
