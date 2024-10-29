a3a73041448f3577be1035d391f41588a8cd90d3
Lee9Bin <imagen33@daum.net>
2024-10-29 17:41:37 +0900

    Feat: default 에러 구현
    ErrorType
    default 서버 에러 정의
    
    S11P31A207-191

--- back/photorize/src/main/java/com/shutter/photorize/global/error/ErrorType.java
+++ back/photorize/src/main/java/com/shutter/photorize/global/error/ErrorType.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorType {
+
+	DEFAULT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에서 예상치 못한 오류가 발생했습니다."),
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
 	; //커스텀 에러 작성
 
