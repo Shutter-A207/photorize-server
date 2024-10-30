70ab4ff88c541f2a6a83247abf7e72853caccb63
Lee9Bin <imagen33@daum.net>
2024-10-31 00:36:11 +0900

    Feat: FileService 구현
    FileService
    S3 업로드 메서드 구현
    추가적인 코드 리팩토링과 구현 필요
    
    S11P31A207-192

--- back/photorize/src/main/java/com/shutter/photorize/domain/file/service/FileService.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/file/service/FileService.java
@@ -1,7 +1,53 @@
 package com.shutter.photorize.domain.file.service;
 
+import java.io.IOException;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.shutter.photorize.global.error.ErrorType;
+import com.shutter.photorize.global.exception.PhotorizeException;
+
+import lombok.RequiredArgsConstructor;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service
+@RequiredArgsConstructor
 public class FileService {
+
+	private final S3Client s3Client;
+
+	@Value("${spring.cloud.aws.s3.bucket}")
+	private String bucket;
+
+	public String uploadFile(MultipartFile file) {
+		String fileName = generateFileName(file);
+
+		uploadToS3(file, fileName, "image/png");
+
+		return String.format("https://%s.s3.amazonaws.com/%s", bucket, fileName);
+	}
+
+	private String generateFileName(MultipartFile file) {
+		return UUID.randomUUID() + "_" + file.getOriginalFilename();
+	}
+
+	private void uploadToS3(MultipartFile file, String fileName, String contentType) {
+		try {
+			s3Client.putObject(
+				PutObjectRequest.builder()
+					.bucket(bucket)
+					.key(fileName)
+					.contentType(contentType)
+					.build(),
+				RequestBody.fromBytes(file.getBytes())
+			);
+		} catch (IOException e) {
+			throw new PhotorizeException(ErrorType.FILE_UPLOAD_ERROR);
+		}
+	}
 }
