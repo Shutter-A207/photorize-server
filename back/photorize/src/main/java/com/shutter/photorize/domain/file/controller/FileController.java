12c5dfe777c35f42dd2a187e0611415f6dd2db86
Lee9Bin <imagen33@daum.net>
2024-10-31 00:38:22 +0900

    Feat: FileController 구현
    FileController
    API 테스트용 FileController 구현
    추후 삭제하기
    
    S11P31A207-192

--- back/photorize/src/main/java/com/shutter/photorize/domain/file/controller/FileController.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/file/controller/FileController.java
@@ -1,7 +1,26 @@
 package com.shutter.photorize.domain.file.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.shutter.photorize.domain.file.service.FileService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping("/api/v1/file")
+@RequiredArgsConstructor
 public class FileController {
+
+	private final FileService fileService;
+
+	//업로드 테스트
+	@PostMapping("/upload")
+	public String uploadFile(@RequestParam("file") MultipartFile file) {
+		String imageUrl = fileService.uploadFile(file);
+		return "imageUrl: " + imageUrl;
+	}
 }
