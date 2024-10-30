ac88efbf9e5e2454f2a6f90f91318a04cfc38b7a
Lee9Bin <imagen33@daum.net>
2024-10-30 10:22:24 +0900

    Feat: 잘못된 리소스 요청 에러 정의
    ErrorType
    잘못된 리소스 요청 에러 정의
    
    S11P31A207-191

--- back/photorize/src/main/java/com/shutter/photorize/global/error/ErrorType.java
+++ back/photorize/src/main/java/com/shutter/photorize/global/error/ErrorType.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorType {
 
 	DEFAULT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에서 예상치 못한 오류가 발생했습니다."),
+	NO_RESOURCE_FOUND(HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다."),
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
 	; //커스텀 에러 작성
 
