b508ca97e605a536374e6087916d34278afbc857
Lee9Bin <imagen33@daum.net>
2024-10-31 14:15:47 +0900

    Refactor: S3Utils와 FileService 역할 구분
    S3, File
    S3Utils와 FileService 역할 구분
    
    S11P31A207-192

--- back/photorize/src/main/java/com/shutter/photorize/domain/file/service/FileService.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/file/service/FileService.java
@@ -1,59 +1,19 @@
 package com.shutter.photorize.domain.file.service;
 
-import java.io.IOException;
-import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.shutter.photorize.global.error.ErrorType;
-import com.shutter.photorize.global.exception.PhotorizeException;
+import com.shutter.photorize.global.util.S3Utils;
 
 import lombok.RequiredArgsConstructor;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service
 @RequiredArgsConstructor
 public class FileService {
 
-	private final S3Client s3Client;
-
-	@Value("${spring.cloud.aws.s3.bucket}")
-	private String bucket;
-
-	@Value("${spring.cloud.aws.region.static}")
-	private String region;
-
-	@Value("${spring.cloud.aws.base-url}")
-	private String s3Url;
+	private final S3Utils s3Utils;
 
 	public String uploadFile(MultipartFile file) {
-		String fileName = generateFileName(file);
-
-		uploadToS3(file, fileName);
-
-		return String.format(s3Url, bucket, region, fileName);
-	}
-
-	private String generateFileName(MultipartFile file) {
-		return UUID.randomUUID() + "_" + file.getOriginalFilename();
-	}
-
-	private void uploadToS3(MultipartFile file, String fileName) {
-		try {
-			s3Client.putObject(
-				PutObjectRequest.builder()
-					.bucket(bucket)
-					.key(fileName)
-					.contentType(file.getContentType())
-					.build(),
-				RequestBody.fromBytes(file.getBytes())
-			);
-		} catch (IOException e) {
-			throw new PhotorizeException(ErrorType.FILE_UPLOAD_ERROR);
-		}
+		return s3Utils.uploadFile(file);
 	}
 }
