f472db2a1f9a2d2afe5eda64f90903ada9714d55
Lee9Bin <imagen33@daum.net>
2024-11-01 15:19:24 +0900

    Feat: 파일 확장자별 type 정의
    File
    파일 확장자별 type 정의 및 확장자 검증 메서드 구현
    
    S11P31A207-206

--- back/photorize/src/main/java/com/shutter/photorize/domain/file/entity/FileType.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/file/entity/FileType.java
@@ -1,5 +1,26 @@
 package com.shutter.photorize.domain.file.entity;
 
+import java.util.Arrays;
+import java.util.Set;
+
+import com.shutter.photorize.global.error.ErrorType;
+import com.shutter.photorize.global.exception.PhotorizeException;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum FileType {
-	PHOTO, VIDEO
+	PHOTO(Set.of("jpg", "jpeg", "png", "bmp", "webp", "heif", "heic", "tiff", "tif")),
+	VIDEO(Set.of("mp4", "mov", "mpeg-4", "avi", "mkv", "webm", "wmv", "flv"));
+
+	private final Set<String> extensions;
+
+	public static FileType fromExtension(String extension) {
+		return Arrays.stream(FileType.values())
+			.filter(fileType -> fileType.extensions.contains(extension.toLowerCase()))
+			.findFirst()
+			.orElseThrow(() -> new PhotorizeException(ErrorType.FILE_UNSUPPORTED_EXTENSION));
+	}
 }
