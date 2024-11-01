67031b2622ca736756b551368f72e3929e806e84
Lee9Bin <imagen33@daum.net>
2024-11-01 14:28:18 +0900

    Feat: file 엔티티 생성 메서드
    File
    엔티티 생성 메서드 구현
    
    S11P31A207-206

--- back/photorize/src/main/java/com/shutter/photorize/domain/file/entity/File.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/file/entity/File.java
@@ -5,6 +5,8 @@ import com.shutter.photorize.global.entity.UpdatableEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -28,9 +31,25 @@ public class File extends UpdatableEntity {
 	@JoinColumn(name = "memory_id", nullable = false)
 	private Memory memory;
 
-	@Column(nullable = false)
-	private String type;
+	@Column(name = "type", columnDefinition = "varchar(20)")
+	@Enumerated(EnumType.STRING)
+	private FileType fileType;
 
 	@Column(nullable = false, unique = true)
 	private String url;
+
+	@Builder
+	private File(Memory memory, FileType fileType, String url) {
+		this.memory = memory;
+		this.fileType = fileType;
+		this.url = url;
+	}
+
+	public static File of(Memory memory, FileType fileType, String url) {
+		return File.builder()
+			.memory(memory)
+			.fileType(fileType)
+			.url(url)
+			.build();
+	}
 }
