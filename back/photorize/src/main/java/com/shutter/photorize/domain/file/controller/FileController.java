a903ce5252ec622034075295ced36dcbdfd1cd22
Lee9Bin <imagen33@daum.net>
2024-11-01 15:59:00 +0900

    Feat: 파일 저장 메서드 구현
    File
    파일 저장 메서드 구현 및 테스트 메서드 제거
    
    S11P31A207-206

--- back/photorize/src/main/java/com/shutter/photorize/domain/file/controller/FileController.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/file/controller/FileController.java
@@ -1,10 +1,7 @@
 package com.shutter.photorize.domain.file.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.shutter.photorize.domain.file.service.FileService;
 
@@ -17,10 +14,4 @@ public class FileController {
 
 	private final FileService fileService;
 
-	//업로드 테스트
-	@PostMapping("/upload")
-	public String uploadFile(@RequestParam("file") MultipartFile file) {
-		String imageUrl = fileService.uploadFile(file);
-		return "imageUrl: " + imageUrl;
-	}
 }
--- back/photorize/src/main/java/com/shutter/photorize/domain/file/service/FileService.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/file/service/FileService.java
@@ -1,8 +1,15 @@
 package com.shutter.photorize.domain.file.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.shutter.photorize.domain.file.entity.File;
+import com.shutter.photorize.domain.file.entity.FileType;
+import com.shutter.photorize.domain.file.repository.FileRepository;
+import com.shutter.photorize.domain.memory.entity.Memory;
 import com.shutter.photorize.global.util.S3Utils;
 
 import lombok.RequiredArgsConstructor;
@@ -11,9 +18,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class FileService {
 
+	private final FileRepository fileRepository;
 	private final S3Utils s3Utils;
 
-	public String uploadFile(MultipartFile file) {
-		return s3Utils.uploadFile(file);
+	public void saveFile(List<MultipartFile> files, Memory memory) {
+		files.forEach(file -> {
+			String extension = getFileExtension(file);
+			FileType type = FileType.fromExtension(extension);
+			String url = s3Utils.uploadFile(file, type);
+			fileRepository.save(File.of(memory, type, url));
+		});
+	}
+
+	private String getFileExtension(MultipartFile file) {
+		return StringUtils.getFilenameExtension(file.getOriginalFilename());
 	}
 }
