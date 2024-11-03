c1b54e058149ca36e668c4e034a733b32dd658ea
Lee9Bin <imagen33@daum.net>
2024-11-03 23:07:44 +0900

    Feat: getOrThrow메서드 구현
    MemoryRepository
    추억 ID 조회 시 존재하지 않으면 예외 던지는 메서드 구현
    findById 메서드를 래핑하여 예외 처리를 간소화
    
    S11P31A207-223

--- back/photorize/src/main/java/com/shutter/photorize/domain/memory/repository/MemoryRepository.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/memory/repository/MemoryRepository.java
@@ -1,24 +1,29 @@
 package com.shutter.photorize.domain.memory.repository;
 
+import com.shutter.photorize.domain.album.entity.Album;
+import com.shutter.photorize.domain.memory.dto.MemoryInfoDto;
+import com.shutter.photorize.domain.memory.entity.Memory;
+import com.shutter.photorize.global.error.ErrorType;
+import com.shutter.photorize.global.exception.PhotorizeException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.shutter.photorize.domain.album.entity.Album;
-import com.shutter.photorize.domain.memory.dto.MemoryInfoDto;
-import com.shutter.photorize.domain.memory.entity.Memory;
-
 @Repository
 public interface MemoryRepository extends JpaRepository<Memory, Long> {
 
-	@Query("SELECT new com.shutter.photorize.domain.memory.dto.MemoryInfoDto(" +
-		"m.id, f.url, s.name, m.date) " +
-		"FROM Memory m " +
-		"LEFT JOIN m.spot s " +
-		"LEFT JOIN File f ON f.memory = m " +
-		"WHERE m.album = :album " +
-		"ORDER BY m.date DESC")
-	Slice<MemoryInfoDto> findMemoryInfoDtosByAlbum(Album album, Pageable pageable);
+    default Memory getOrThrow(Long id) {
+        return findById(id).orElseThrow(() -> new PhotorizeException(ErrorType.MEMORY_NOT_FOUND));
+    }
+
+    @Query("SELECT new com.shutter.photorize.domain.memory.dto.MemoryInfoDto(" +
+            "m.id, f.url, s.name, m.date) " +
+            "FROM Memory m " +
+            "LEFT JOIN m.spot s " +
+            "LEFT JOIN File f ON f.memory = m " +
+            "WHERE m.album = :album " +
+            "ORDER BY m.date DESC")
+    Slice<MemoryInfoDto> findMemoryInfoDtosByAlbum(Album album, Pageable pageable);
 }
