3efa1983edcec69edc7feab5d863c2d567c9247c
Lee9Bin <imagen33@daum.net>
2024-11-04 15:29:10 +0900

    Feat: 댓글과 작성자 정보를 페이징 처리하여 조회하는 메서드 구현
    Comment
    Memory를 기준으로 댓글과 작성자(Member)를 최신순으로 조회하는 findCommentsWithMemberByMemory 메서드를 추가했습니다.
    
    S11P31A207-223

--- back/photorize/src/main/java/com/shutter/photorize/domain/comment/repository/CommentRepository.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/comment/repository/CommentRepository.java
@@ -1,10 +1,18 @@
 package com.shutter.photorize.domain.comment.repository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.shutter.photorize.domain.comment.entity.Comment;
+import com.shutter.photorize.domain.memory.entity.Memory;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+	@Query("SELECT c FROM Comment c JOIN FETCH c.member WHERE c.memory = :memory ORDER BY c.createdAt DESC")
+	Slice<Comment> findCommentsWithMemberByMemory(@Param("memory") Memory memory, Pageable pageable);
 }
