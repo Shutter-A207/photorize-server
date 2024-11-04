cf378d9bcb0f4daab3b2b20a94dd39817d6edef2
Lee9Bin <imagen33@daum.net>
2024-11-04 15:26:28 +0900

    Feat: COMMENT_PAGE_SIZE 추가
    Number
    댓글 페이지네이션 기본 사이즈 정의
    
    S11P31A207-223

--- back/photorize/src/main/java/com/shutter/photorize/global/constant/Number.java
+++ back/photorize/src/main/java/com/shutter/photorize/global/constant/Number.java
@@ -3,4 +3,5 @@ package com.shutter.photorize.global.constant;
 public class Number {
 	public static final int ALBUM_BOARD_PAGE_SIZE = 8;
 	public static final int ALBUM_DETAIL_PAGE_SIZE = 8;
+	public static final int COMMENT_PAGE_SIZE = 8;
 }
