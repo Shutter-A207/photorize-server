e403d3dfb69e3d6939d90d1a91a5d234ba01d0f2
Lee9Bin <imagen33@daum.net>
2024-11-04 15:31:54 +0900

    Feat: 댓글 응답 DTO 구현
    comment
    댓글 응답용 DTO 구현
    
    S11P31A207-223

--- back/photorize/src/main/java/com/shutter/photorize/domain/comment/dto/response/CommentResponse.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/comment/dto/response/CommentResponse.java
@@ -1,18 +1,45 @@
 package com.shutter.photorize.domain.comment.dto.response;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.shutter.photorize.domain.comment.entity.Comment;
+
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor
 public class CommentResponse {
 
-    private Long commentId;
-    private Long writerId;
-    private String writerImg;
-    private String nickname;
-    private String content;
-    private LocalDate date;
+	private Long commentId;
+	private Long writerId;
+	private String writerImg;
+	private String nickname;
+	private String content;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	private LocalDateTime date;
+
+	@Builder
+	private CommentResponse(Long commentId, Long writerId, String writerImg, String nickname, String content,
+		LocalDateTime date) {
+		this.commentId = commentId;
+		this.writerId = writerId;
+		this.writerImg = writerImg;
+		this.nickname = nickname;
+		this.content = content;
+		this.date = date;
+	}
+
+	public static CommentResponse from(Comment comment) {
+		return CommentResponse.builder()
+			.commentId(comment.getId())
+			.writerId(comment.getMember().getId())
+			.writerImg(comment.getMember().getImg())
+			.nickname(comment.getMember().getNickname())
+			.content(comment.getContent())
+			.date(comment.getCreatedAt())
+			.build();
+	}
 }
