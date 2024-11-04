8337be36e0590f67342cf81d0b8080924697d7aa
Lee9Bin <imagen33@daum.net>
2024-11-04 15:34:22 +0900

    Feat: 댓글 조회 메서드 구현
    CommentService
    댓글 조회 메서드 구현
    
    S11P31A207-223

--- back/photorize/src/main/java/com/shutter/photorize/domain/comment/service/CommentService.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/comment/service/CommentService.java
@@ -1,7 +1,25 @@
 package com.shutter.photorize.domain.comment.service;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
+import com.shutter.photorize.domain.comment.dto.response.CommentResponse;
+import com.shutter.photorize.domain.comment.repository.CommentRepository;
+import com.shutter.photorize.domain.memory.entity.Memory;
+import com.shutter.photorize.global.response.SliceResponse;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class CommentService {
+
+	private final CommentRepository commentRepository;
+
+	public SliceResponse<CommentResponse> findCommentsWithMemberByMemory(Memory memory, Pageable pageable) {
+		Slice<CommentResponse> comments = commentRepository.findCommentsWithMemberByMemory(memory, pageable)
+			.map(CommentResponse::from);
+		return SliceResponse.of(comments);
+	}
 }
