09b8c029375de1a3d6bf33835f44a62245643fc2
Lee9Bin <imagen33@daum.net>
2024-11-04 15:38:26 +0900

    Feat: File 조회 메서드 구현
    FileRepository
    memory에 해당하는 file 조회 메서드 구현
    
    S11P31A207-223

--- back/photorize/src/main/java/com/shutter/photorize/domain/file/repository/FileRepository.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/file/repository/FileRepository.java
@@ -1,10 +1,15 @@
 package com.shutter.photorize.domain.file.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.shutter.photorize.domain.file.entity.File;
+import com.shutter.photorize.domain.memory.entity.Memory;
 
 @Repository
 public interface FileRepository extends JpaRepository<File, Long> {
+
+	List<File> findFilesByMemory(Memory memory);
 }
