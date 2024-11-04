6b3145cbdf2bb946154ece34d5afe5c9575ae29d
Lee9Bin <imagen33@daum.net>
2024-11-04 15:43:19 +0900

    Feat: 추억 응답 DTO 구현
    MemoryResponse
    추억 응답 DTO 구현
    
    S11P31A207-223

--- back/photorize/src/main/java/com/shutter/photorize/domain/memory/dto/response/MemoryResponse.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/memory/dto/response/MemoryResponse.java
@@ -1,18 +1,48 @@
 package com.shutter.photorize.domain.memory.dto.response;
 
-import lombok.Getter;
-
 import java.time.LocalDate;
 import java.util.List;
 
+import com.shutter.photorize.domain.file.dto.response.FileResponse;
+import com.shutter.photorize.domain.memory.entity.Memory;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 @Getter
+@NoArgsConstructor
 public class MemoryResponse {
 
-    private Long writerId;
-    private String nickname;
-    private String writerImg;
-    private List<String> files;
-    private LocalDate date;
-    private String spotName;
-    private String content;
+	private Long writerId;
+	private String nickname;
+	private String writerImg;
+	private List<FileResponse> files;
+	private LocalDate date;
+	private String spotName;
+	private String content;
+
+	@Builder
+	private MemoryResponse(Long writerId, String nickname, String writerImg, List<FileResponse> files, LocalDate date,
+		String spotName, String content) {
+		this.writerId = writerId;
+		this.nickname = nickname;
+		this.writerImg = writerImg;
+		this.files = files;
+		this.date = date;
+		this.spotName = spotName;
+		this.content = content;
+	}
+
+	public static MemoryResponse of(Memory memory, List<FileResponse> files) {
+		return MemoryResponse.builder()
+			.writerId(memory.getMember().getId())
+			.nickname(memory.getMember().getNickname())
+			.writerImg(memory.getMember().getImg())
+			.files(files)
+			.date(LocalDate.from(memory.getDate()))
+			.spotName(memory.getSpot().getName())
+			.content(memory.getContent())
+			.build();
+	}
 }
