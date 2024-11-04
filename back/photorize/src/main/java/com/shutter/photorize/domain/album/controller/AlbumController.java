403f575bb6089e8f224f431a6dae8dcbf084de0a
최은혜 <antlf1104@naver.com>
2024-11-04 11:05:47 +0900

    Refactor : 어노테이션 변경

--- back/photorize/src/main/java/com/shutter/photorize/domain/album/controller/AlbumController.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/album/controller/AlbumController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shutter.photorize.domain.album.dto.request.AlbumCreateRequest;
@@ -34,7 +33,7 @@ public class AlbumController {
 
 	@PostMapping
 	public ResponseEntity<ApiResponse<Void>> createAlbum(
-		@RequestPart AlbumCreateRequest albumCreateRequest) {
+		@RequestBody AlbumCreateRequest albumCreateRequest) {
 		albumService.createPublicAlbum(albumCreateRequest, 1L);
 		return ApiResponse.created();
 	}
