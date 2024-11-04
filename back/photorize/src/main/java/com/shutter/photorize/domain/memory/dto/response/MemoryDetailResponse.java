5508f108c20cd65cfb4d4665f4b70abcb581426a
Lee9Bin <imagen33@daum.net>
2024-11-04 15:44:18 +0900

    Feat: 추억 조회 응답 DTO 구현
    MemoryDetailResponse
    추억 데이터와 댓글 목록 조회 응답 DTO 구현
    
    S11P31A207-223

--- back/photorize/src/main/java/com/shutter/photorize/domain/memory/dto/response/MemoryDetailResponse.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/memory/dto/response/MemoryDetailResponse.java
@@ -1,16 +1,29 @@
 package com.shutter.photorize.domain.memory.dto.response;
 
 import com.shutter.photorize.domain.comment.dto.response.CommentResponse;
-import com.shutter.photorize.domain.memory.repository.MemoryRepository;
+import com.shutter.photorize.global.response.SliceResponse;
+
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class MemoryDetailResponse {
-    
-    private MemoryRepository memory;
-    private List<CommentResponse> comments;
+
+	private MemoryResponse memory;
+	private SliceResponse<CommentResponse> comments;
+
+	@Builder
+	private MemoryDetailResponse(MemoryResponse memory, SliceResponse<CommentResponse> comments) {
+		this.memory = memory;
+		this.comments = comments;
+	}
+
+	public static MemoryDetailResponse of(MemoryResponse memory, SliceResponse<CommentResponse> comments) {
+		return MemoryDetailResponse.builder()
+			.memory(memory)
+			.comments(comments)
+			.build();
+	}
 }
