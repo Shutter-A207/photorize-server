6aa905e83c400e6e2879da97e69d85fb0e4359e3
Lee9Bin <imagen33@daum.net>
2024-11-05 17:27:44 +0900

    Feat: 댓글 생성, 수정, 삭제 API 구현
    Comment
    댓글 생성, 수정, 삭제 API 구현과 댓글 작성자와 일치 여부 메서드 구현
    
    S11P31A207-241

--- back/photorize/src/main/java/com/shutter/photorize/domain/comment/controller/CommentController.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/comment/controller/CommentController.java
@@ -1,7 +1,44 @@
 package com.shutter.photorize.domain.comment.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shutter.photorize.domain.comment.dto.request.CommentCreateRequest;
+import com.shutter.photorize.domain.comment.dto.request.CommentUpdateRequest;
+import com.shutter.photorize.domain.comment.service.CommentService;
+import com.shutter.photorize.global.response.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequestMapping("/api/v1/comments")
+@RequiredArgsConstructor
 public class CommentController {
+
+	private final CommentService commentService;
+
+	@PostMapping
+	public ResponseEntity<ApiResponse<Void>> createComment(@RequestBody CommentCreateRequest commentCreateRequest) {
+		//TODO: 하드 코딩 수정해야합니다.
+		commentService.createComment(commentCreateRequest, 1L);
+		return ApiResponse.created();
+	}
+
+	@PostMapping("/{commentId}")
+	public ResponseEntity<ApiResponse<Void>> updateComment(@PathVariable Long commentId,
+		@RequestBody CommentUpdateRequest commentUpdateRequest) {
+		commentService.updateComment(commentId, commentUpdateRequest, 1L);
+		return ApiResponse.ok(null);
+	}
+
+	@DeleteMapping("/{commentId}")
+	public ResponseEntity<ApiResponse<Void>> deleteComment(@PathVariable Long commentId) {
+		commentService.deleteComment(commentId, 1L);
+		return ApiResponse.ok(null);
+	}
 }
--- back/photorize/src/main/java/com/shutter/photorize/domain/comment/service/CommentService.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/comment/service/CommentService.java
@@ -3,10 +3,19 @@ package com.shutter.photorize.domain.comment.service;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.shutter.photorize.domain.comment.dto.request.CommentCreateRequest;
+import com.shutter.photorize.domain.comment.dto.request.CommentUpdateRequest;
 import com.shutter.photorize.domain.comment.dto.response.CommentResponse;
+import com.shutter.photorize.domain.comment.entity.Comment;
 import com.shutter.photorize.domain.comment.repository.CommentRepository;
+import com.shutter.photorize.domain.member.entity.Member;
+import com.shutter.photorize.domain.member.repository.MemberRepository;
 import com.shutter.photorize.domain.memory.entity.Memory;
+import com.shutter.photorize.domain.memory.repository.MemoryRepository;
+import com.shutter.photorize.global.error.ErrorType;
+import com.shutter.photorize.global.exception.PhotorizeException;
 import com.shutter.photorize.global.response.SliceResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -16,10 +25,45 @@ import lombok.RequiredArgsConstructor;
 public class CommentService {
 
 	private final CommentRepository commentRepository;
+	private final MemberRepository memberRepository;
+	private final MemoryRepository memoryRepository;
 
 	public SliceResponse<CommentResponse> findCommentsWithMemberByMemory(Memory memory, Pageable pageable) {
 		Slice<CommentResponse> comments = commentRepository.findCommentsWithMemberByMemory(memory, pageable)
 			.map(CommentResponse::from);
 		return SliceResponse.of(comments);
 	}
+
+	@Transactional
+	public void createComment(CommentCreateRequest commentCreateRequest, Long memberId) {
+		Member member = memberRepository.getOrThrow(memberId);
+		Memory memory = memoryRepository.getOrThrow(commentCreateRequest.getMemoryId());
+
+		commentRepository.save(commentCreateRequest.toComment(member, memory));
+	}
+
+	@Transactional
+	public void updateComment(Long commentId, CommentUpdateRequest commentUpdateRequest, Long memberId) {
+		Comment comment = commentRepository.getOrThrow(commentId);
+
+		validateAuthor(comment, memberId);
+
+		comment.updateContent(commentUpdateRequest.getContent());
+	}
+
+	@Transactional
+	public void deleteComment(Long commentId, Long memberId) {
+		Comment comment = commentRepository.getOrThrow(commentId);
+
+		validateAuthor(comment, memberId);
+
+		commentRepository.delete(comment);
+	}
+
+	private void validateAuthor(Comment comment, Long memberId) {
+		if (!comment.getMember().getId().equals(memberId)) {
+			throw new PhotorizeException(ErrorType.COMMENT_FORBIDDEN);
+		}
+	}
+
 }
