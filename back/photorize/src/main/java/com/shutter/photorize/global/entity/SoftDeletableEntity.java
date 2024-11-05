1cd6df4a55902268da265557f55b81a9bef721da
Lee9Bin <imagen33@daum.net>
2024-11-05 10:55:29 +0900

    Feat: @SQLRestriction 제거
    SoftDeletableEntity
    상속을 활용해 @SQLRestriction 관리가 되지 않아서 softdelete 대상 객체에 어노테이션 붙이기
    
    S11P31A207-241

--- back/photorize/src/main/java/com/shutter/photorize/global/entity/SoftDeletableEntity.java
+++ back/photorize/src/main/java/com/shutter/photorize/global/entity/SoftDeletableEntity.java
@@ -2,15 +2,12 @@ package com.shutter.photorize.global.entity;
 
 import java.time.LocalDateTime;
 
-import org.hibernate.annotations.SQLRestriction;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
 @MappedSuperclass
 @Getter
-@SQLRestriction("deleted_at is NULL")
 public abstract class SoftDeletableEntity extends UpdatableEntity {
 
 	@Column(columnDefinition = "TIMESTAMP")
