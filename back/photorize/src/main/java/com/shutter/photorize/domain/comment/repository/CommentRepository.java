ec625c84a02791b3b4b2145d71d2675534660f4d
Lee9Bin <imagen33@daum.net>
2024-11-05 17:23:48 +0900

    Feat: default 메서드 구현
    CommentRepository
    findById default 메서드 구현
    
    S11P31A207-241

--- back/photorize/src/main/java/com/shutter/photorize/domain/comment/repository/CommentRepository.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/comment/repository/CommentRepository.java
@@ -9,9 +9,15 @@ import org.springframework.stereotype.Repository;
 
 import com.shutter.photorize.domain.comment.entity.Comment;
 import com.shutter.photorize.domain.memory.entity.Memory;
+import com.shutter.photorize.global.error.ErrorType;
+import com.shutter.photorize.global.exception.PhotorizeException;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+	default Comment getOrThrow(Long id) {
+		return findById(id).orElseThrow(() -> new PhotorizeException(ErrorType.COMMENT_NOT_FOUND));
+	}
 
 	@Query("SELECT c FROM Comment c JOIN FETCH c.member WHERE c.memory = :memory ORDER BY c.createdAt DESC")
 	Slice<Comment> findCommentsWithMemberByMemory(@Param("memory") Memory memory, Pageable pageable);
