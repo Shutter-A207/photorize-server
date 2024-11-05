f482ca8fdf541f884baecf2b48a019725e0e7d21
Lee9Bin <imagen33@daum.net>
2024-11-05 14:26:54 +0900

    Feat: type과 memory에 해당 하는 File 조회 메서드 구현
    FileRepository
    type 과 memory에 해당하는 file 조회 메서드 구현
    
    S11P31A207-241

--- back/photorize/src/main/java/com/shutter/photorize/domain/file/repository/FileRepository.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/file/repository/FileRepository.java
@@ -1,15 +1,26 @@
 package com.shutter.photorize.domain.file.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.shutter.photorize.domain.file.entity.File;
+import com.shutter.photorize.domain.file.entity.FileType;
 import com.shutter.photorize.domain.memory.entity.Memory;
+import com.shutter.photorize.global.error.ErrorType;
+import com.shutter.photorize.global.exception.PhotorizeException;
 
 @Repository
 public interface FileRepository extends JpaRepository<File, Long> {
 
+	default File getFilesByMemoryAndTypeOrThrow(Memory memory, FileType type) {
+		return findFilesByMemoryAndType(memory, type).orElseThrow(
+			() -> new PhotorizeException(ErrorType.FILE_NOT_FOUND));
+	}
+
 	List<File> findFilesByMemory(Memory memory);
+
+	Optional<File> findFilesByMemoryAndType(Memory memory, FileType type);
 }
