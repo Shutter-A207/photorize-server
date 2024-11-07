9157af465c3bb39739542d72eab2900a73c9fc6b
박진형 <dyunames21@gmail.com>
2024-11-07 09:30:09 +0900

    Feat : SPOT API - 스팟 클릭시, 유저의 해당하는 스팟에 해당하는 파일과 파일이 해당하는 메모리의 ID 뿐만 아니라 메모리 정보에 해당하는 다른 칼럼도 전달.

--- back/photorize/src/main/java/com/shutter/photorize/domain/spot/dto/response/SpotFileResponse.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/spot/dto/response/SpotFileResponse.java
@@ -1,5 +1,7 @@
 package com.shutter.photorize.domain.spot.dto.response;
 
+import java.time.LocalDateTime;
+
 import com.shutter.photorize.domain.file.entity.File;
 
 import lombok.Getter;
@@ -11,12 +13,17 @@ public class SpotFileResponse {
 	private Long memoryId;
 	private String fileUrl;
 	private String fileType;
+	private String spotName;
+	private LocalDateTime date;
 
-	private SpotFileResponse(Long fileId, Long memoryId, String fileUrl, String fileType) {
+	private SpotFileResponse(Long fileId, Long memoryId, String fileUrl, String fileType, String spotName,
+		LocalDateTime date) {
 		this.fileId = fileId;
 		this.memoryId = memoryId;
 		this.fileUrl = fileUrl;
 		this.fileType = fileType;
+		this.spotName = spotName;
+		this.date = date;
 	}
 
 	public static SpotFileResponse of(File file) {
@@ -24,7 +31,9 @@ public class SpotFileResponse {
 			file.getId(),
 			file.getMemory().getId(),
 			file.getUrl(),
-			file.getType().name()
+			file.getType().name(),
+			file.getMemory().getSpot().getName(),
+			file.getMemory().getCreatedAt()
 		);
 	}
 }
