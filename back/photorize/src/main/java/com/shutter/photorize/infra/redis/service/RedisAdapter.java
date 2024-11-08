76271bd47193c4212b9ae2c7b6983c470a1ec538
최은혜 <antlf1104@naver.com>
2024-11-08 17:49:03 +0900

    Feat: redis 설정

---
+++ back/photorize/src/main/java/com/shutter/photorize/infra/redis/service/RedisAdapter.java
@@ -0,0 +1,11 @@
+package com.shutter.photorize.infra.redis.service;
+
+import java.util.Optional;
+
+public interface RedisAdapter {
+	void saveOrUpdate(String key, String value);
+
+	Optional<String> getValue(String key);
+
+	void delete(String key);
+}
---
+++ back/photorize/src/main/java/com/shutter/photorize/infra/redis/service/RedisAuthCodeAdapter.java
@@ -0,0 +1,43 @@
+package com.shutter.photorize.infra.redis.service;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class RedisAuthCodeAdapter implements RedisAdapter {
+
+	private final RedisTemplate<String, Object> redisTemplate;
+
+	// key-value를 redis에 저장
+	@Override
+	public void saveOrUpdate(String key, String value) {
+		redisTemplate.opsForValue().set(key, value);
+	}
+
+	// 만료 시간 설정 저장 메소드
+	public void saveOrUpdate(String key, String value, int min) {
+		redisTemplate.opsForValue().set(key, value, min, TimeUnit.MINUTES);
+	}
+
+	// 값 조회 메소드
+	@Override
+	public Optional<String> getValue(String key) {
+		Object value = redisTemplate.opsForValue().get(key);
+		if (value instanceof String) {
+			return Optional.of((String)value);
+		}
+		return Optional.empty();
+	}
+
+	// 주어진 key에 해당하는 데이터 삭제
+	@Override
+	public void delete(String key) {
+		redisTemplate.delete(key);
+	}
+}
