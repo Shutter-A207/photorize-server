d5914cda05d0965700b482dcebd5f29159a5680d
최은혜 <antlf1104@naver.com>
2024-11-08 17:52:24 +0900

    Feat: 이메일 인증 및 비밀번호 변경 API 구현중

--- back/photorize/src/main/java/com/shutter/photorize/domain/member/controller/AuthController.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/member/controller/AuthController.java
@@ -1,14 +1,20 @@
 package com.shutter.photorize.domain.member.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shutter.photorize.domain.album.service.AlbumService;
+import com.shutter.photorize.domain.member.dto.request.EmailAuthRequest;
 import com.shutter.photorize.domain.member.dto.request.JoinRequest;
+import com.shutter.photorize.domain.member.service.AuthService;
 import com.shutter.photorize.domain.member.service.MemberService;
+import com.shutter.photorize.domain.member.strategy.AuthCodeType;
 import com.shutter.photorize.global.response.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -20,11 +26,27 @@ public class AuthController {
 
 	private final MemberService memberService;
 	private final AlbumService albumService;
+	private final AuthService authService;
 
 	@PostMapping("/join")
 	public ResponseEntity<ApiResponse<Boolean>> createMember(@RequestBody JoinRequest joinRequest) {
 		Long memberId = memberService.createMember(joinRequest);
 		albumService.createPrivateAlbum(memberId);
+
+		return ApiResponse.created();
+	}
+
+	@GetMapping("/email/code/{email}")
+	public ResponseEntity<ApiResponse<Boolean>> createEmailAuthCode(@PathVariable String email,
+		@RequestParam String type) {
+
+		authService.createEmailAuthCode(email, AuthCodeType.of(type));
+		return ApiResponse.created();
+	}
+
+	@PostMapping("/email/code")
+	public ResponseEntity<ApiResponse<Boolean>> validEmailAuthCode(@RequestBody EmailAuthRequest emailAuthRequest) {
+		authService.validAuthCode(emailAuthRequest, AuthCodeType.of(String.valueOf(emailAuthRequest.getAuthType())));
 
 		return ApiResponse.created();
 	}
---
+++ back/photorize/src/main/java/com/shutter/photorize/domain/member/service/AuthService.java
@@ -0,0 +1,32 @@
+package com.shutter.photorize.domain.member.service;
+
+import org.springframework.stereotype.Service;
+
+import com.shutter.photorize.domain.member.dto.request.EmailAuthRequest;
+import com.shutter.photorize.domain.member.strategy.AuthCodeType;
+import com.shutter.photorize.infra.mail.model.EmailForm;
+import com.shutter.photorize.infra.mail.service.MailService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+	private final AuthCodeService authCodeService;
+	private final MailService mailService;
+
+	public boolean createEmailAuthCode(String email, AuthCodeType authCodeType) {
+		String code = authCodeService.createAuthCode(email, authCodeType);
+
+		EmailForm emailForm = authCodeService.getAuthEmailForm(email, code, authCodeType);
+
+		mailService.sendEmail(emailForm.getTo(), emailForm.getSubject(), emailForm.getContent(), emailForm.isHtml());
+
+		return true;
+	}
+
+	public boolean validAuthCode(EmailAuthRequest emailAuthRequest, AuthCodeType authCodeType) {
+		return authCodeService.checkValidCode(emailAuthRequest, authCodeType);
+	}
+}
