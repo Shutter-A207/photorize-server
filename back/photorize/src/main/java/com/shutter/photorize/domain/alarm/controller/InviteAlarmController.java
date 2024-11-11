8f1bfbaaea4361a2f9ce680f3422eb0ff9c5b4b7
Lee9Bin <imagen33@daum.net>
2024-11-11 16:12:38 +0900

    Feat: 알림 조회 및 수락/거절 메서드 구현
    InviteAlarmController
    알림 조회 및 수락/거절 메서드 구현
    
    S11P31A207-383

--- back/photorize/src/main/java/com/shutter/photorize/domain/alarm/controller/InviteAlarmController.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/alarm/controller/InviteAlarmController.java
@@ -1,7 +1,45 @@
 package com.shutter.photorize.domain.alarm.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shutter.photorize.domain.alarm.dto.request.InviteAlarmRequest;
+import com.shutter.photorize.domain.alarm.dto.response.InviteAlarmResponse;
+import com.shutter.photorize.domain.alarm.service.InviteAlarmService;
+import com.shutter.photorize.global.jwt.model.ContextMember;
+import com.shutter.photorize.global.response.ApiResponse;
+import com.shutter.photorize.global.security.AuthUser;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequestMapping("/api/v1/alarms")
+@RequiredArgsConstructor
 public class InviteAlarmController {
+
+	private final InviteAlarmService inviteAlarmService;
+
+	@GetMapping
+	public ResponseEntity<ApiResponse<List<InviteAlarmResponse>>> getInviteAlarms(
+		@AuthUser ContextMember contextMember) {
+		List<InviteAlarmResponse> alarms = inviteAlarmService.getInviteAlarms(contextMember.getId());
+		return ApiResponse.ok(alarms);
+	}
+
+	@PostMapping("{alarmId}")
+	public ResponseEntity<ApiResponse<Void>> respondToInviteAlarm(
+		@PathVariable Long alarmId,
+		@RequestBody InviteAlarmRequest inviteAlarmRequest,
+		@AuthUser ContextMember contextMember) {
+
+		inviteAlarmService.respondToInviteAlarm(contextMember.getId(), alarmId, inviteAlarmRequest);
+		return ApiResponse.ok(null);
+	}
 }
