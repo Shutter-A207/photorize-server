7519e217695dc57744f419d3309f509ee680be24
Lee9Bin <imagen33@daum.net>
2024-11-11 17:04:14 +0900

    Feat: 알람 페이지 갯수 추가
    CommonConstants
    알람 페이지 갯수 추가
    
    S11P31A207-383

--- back/photorize/src/main/java/com/shutter/photorize/global/constant/CommonConstants.java
+++ back/photorize/src/main/java/com/shutter/photorize/global/constant/CommonConstants.java
@@ -4,6 +4,7 @@ public class CommonConstants {
 	public static final int ALBUM_BOARD_PAGE_SIZE = 8;
 	public static final int ALBUM_DETAIL_PAGE_SIZE = 8;
 	public static final int COMMENT_PAGE_SIZE = 8;
+	public static final int ALARM_PAGE_SIZE = 8;
 	public static final long DEFAULT_ALBUM_COLOR = 1L;
 
 }
