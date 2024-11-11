d8b8843d9845364ef6b25016948821abd9197957
Lee9Bin <imagen33@daum.net>
2024-11-11 16:06:12 +0900

    Feat: 회원 토큰 조회 메서드 구현
    FCMRepository
    회원 토큰 조회 메서드 구현
    
    S11P31A207-383

--- back/photorize/src/main/java/com/shutter/photorize/domain/alarm/repository/FCMRepository.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/alarm/repository/FCMRepository.java
@@ -1,11 +1,15 @@
 package com.shutter.photorize.domain.alarm.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.shutter.photorize.domain.alarm.entity.FCMToken;
+import com.shutter.photorize.domain.member.entity.Member;
 
 @Repository
 public interface FCMRepository extends JpaRepository<FCMToken, Long> {
 
+	List<FCMToken> findByMember(Member member);
 }
