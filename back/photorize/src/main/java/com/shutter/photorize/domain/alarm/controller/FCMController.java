4ba173e29c02fe34773ed30afb18b87f80b8e6b1
Lee9Bin <imagen33@daum.net>
2024-11-11 10:40:28 +0900

    Feat: 테스트 메서드 삭제
    FCMController
    알림 보내기 테스트 메서드 삭제
    
    S11P31A207-272

--- back/photorize/src/main/java/com/shutter/photorize/domain/alarm/controller/FCMController.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/alarm/controller/FCMController.java
@@ -2,14 +2,10 @@ package com.shutter.photorize.domain.alarm.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.shutter.photorize.domain.alarm.dto.request.FCMTokenSaveRequest;
 import com.shutter.photorize.domain.alarm.service.FCMService;
-import com.shutter.photorize.domain.album.entity.Album;
-import com.shutter.photorize.domain.album.repository.AlbumRepository;
 import com.shutter.photorize.global.response.ApiResponse;
 import com.shutter.photorize.global.security.AuthUser;
 
@@ -21,19 +17,10 @@ import lombok.RequiredArgsConstructor;
 public class FCMController {
 
 	private final FCMService fcmService;
-	private final AlbumRepository albumRepository;
 
 	public ResponseEntity<ApiResponse<Void>> saveToken(@AuthUser Long memberId,
 		FCMTokenSaveRequest fcmTokenSaveRequest) {
 		fcmService.saveToken(memberId, fcmTokenSaveRequest);
-		return ApiResponse.created();
-	}
-
-	@PostMapping("/send")
-	public ResponseEntity<ApiResponse<Void>> sendMessage(@RequestBody FCMTokenSaveRequest fcmTokenSaveRequest) {
-		//test
-		Album album = albumRepository.getOrThrow(1L);
-		fcmService.sendPublicAlarm(fcmTokenSaveRequest.getToken(), album);
 		return ApiResponse.created();
 	}
 }
