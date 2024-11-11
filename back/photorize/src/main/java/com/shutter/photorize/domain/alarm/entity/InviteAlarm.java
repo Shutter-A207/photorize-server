e85befcb8355e51f2bc9755673e014292b8f7daf
Lee9Bin <imagen33@daum.net>
2024-11-11 16:09:07 +0900

    Feat: InviteAlarm 객체 생성 메서드 구현
    InviteAlarm
    InviteAlarm 객체 생성 메서드 구현
    
    S11P31A207-383

--- back/photorize/src/main/java/com/shutter/photorize/domain/alarm/entity/InviteAlarm.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/alarm/entity/InviteAlarm.java
@@ -1,12 +1,16 @@
 package com.shutter.photorize.domain.alarm.entity;
 
+import org.hibernate.annotations.SQLRestriction;
+
 import com.shutter.photorize.domain.album.entity.Album;
 import com.shutter.photorize.domain.member.entity.Member;
 import com.shutter.photorize.domain.memory.entity.Memory;
-import com.shutter.photorize.global.entity.BaseEntity;
+import com.shutter.photorize.global.entity.SoftDeletableEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,13 +18,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class InviteAlarm extends BaseEntity {
+@SQLRestriction("deleted_at is NULL")
+public class InviteAlarm extends SoftDeletableEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,5 +48,31 @@ public class InviteAlarm extends BaseEntity {
 	private boolean checked;
 
 	@Column(nullable = false)
-	private String type;
+	@Enumerated(EnumType.STRING)
+	private AlarmType type;
+
+	@Builder
+	private InviteAlarm(Long id, Member member, Album album, Memory memory, boolean checked, AlarmType type) {
+		this.id = id;
+		this.member = member;
+		this.memory = memory;
+		this.checked = checked;
+		this.type = type;
+	}
+
+	public static InviteAlarm of(Member member, Memory memory) {
+		return InviteAlarm.builder()
+			.member(member)
+			.memory(memory)
+			.type(AlarmType.PRIVATE)
+			.build();
+	}
+
+	public static InviteAlarm of(Member member, Album album) {
+		return InviteAlarm.builder()
+			.member(member)
+			.album(album)
+			.type(AlarmType.PUBLIC)
+			.build();
+	}
 }
