f62f983338c1ab317b51709a4f07a12b5d7769ed
Lee9Bin <imagen33@daum.net>
2024-11-13 23:08:14 +0900

    Feat: 알람, 추억, 앨범 조인 메서드 구현
    InviteAlarmRepository
    조인 메서드 생성
    
    S11P31A207-403

--- back/photorize/src/main/java/com/shutter/photorize/domain/alarm/repository/InviteAlarmRepository.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/alarm/repository/InviteAlarmRepository.java
@@ -1,7 +1,9 @@
 package com.shutter.photorize.domain.alarm.repository;
 
-import java.util.Optional;
-
+import com.shutter.photorize.domain.alarm.entity.InviteAlarm;
+import com.shutter.photorize.domain.member.entity.Member;
+import com.shutter.photorize.global.error.ErrorType;
+import com.shutter.photorize.global.exception.PhotorizeException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,37 +11,43 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.shutter.photorize.domain.alarm.entity.InviteAlarm;
-import com.shutter.photorize.domain.member.entity.Member;
-import com.shutter.photorize.global.error.ErrorType;
-import com.shutter.photorize.global.exception.PhotorizeException;
+import java.util.Optional;
 
 @Repository
 public interface InviteAlarmRepository extends JpaRepository<InviteAlarm, Long> {
 
-	default InviteAlarm getByIdOrThrow(Long id) {
-		return findById(id).orElseThrow(
-			() -> new PhotorizeException(ErrorType.ALARM_NOT_FOUND));
-	}
+    default InviteAlarm getByIdOrThrow(Long id) {
+        return findById(id).orElseThrow(
+                () -> new PhotorizeException(ErrorType.ALARM_NOT_FOUND));
+    }
 
-	default InviteAlarm getByIdWithMemberAndAlbumAndMemoryOrThrow(Long id) {
-		return findByIdWithMemberAndAlbumAndMemory(id).orElseThrow(
-			() -> new PhotorizeException(ErrorType.ALARM_NOT_FOUND));
-	}
+    default InviteAlarm getByIdWithMemberAndAlbumAndMemoryOrThrow(Long id) {
+        return findByIdWithMemberAndAlbumAndMemory(id).orElseThrow(
+                () -> new PhotorizeException(ErrorType.ALARM_NOT_FOUND));
+    }
 
-	@Query("SELECT ia FROM InviteAlarm ia " +
-		"JOIN FETCH ia.member " +
-		"LEFT JOIN FETCH ia.album album " +
-		"LEFT JOIN FETCH album.member " +
-		"LEFT JOIN FETCH ia.memory memory " +
-		"WHERE ia.member = :member " +
-		"ORDER BY ia.createdAt DESC")
-	Slice<InviteAlarm> findByMember(@Param("member") Member member, Pageable pageable);
+    @Query("SELECT ia FROM InviteAlarm ia " +
+            "JOIN FETCH ia.member " +
+            "LEFT JOIN FETCH ia.album album " +
+            "LEFT JOIN FETCH album.member " +
+            "LEFT JOIN FETCH ia.memory memory " +
+            "WHERE ia.member = :member " +
+            "ORDER BY ia.createdAt DESC")
+    Slice<InviteAlarm> findByMember(@Param("member") Member member, Pageable pageable);
 
-	@Query("SELECT ia FROM InviteAlarm ia " +
-		"LEFT JOIN FETCH ia.member " +
-		"LEFT JOIN FETCH ia.album " +
-		"LEFT JOIN FETCH ia.memory " +
-		"WHERE ia.id = :alarmId")
-	Optional<InviteAlarm> findByIdWithMemberAndAlbumAndMemory(@Param("alarmId") Long alarmId);
+    @Query("SELECT ia FROM InviteAlarm ia " +
+            "LEFT JOIN FETCH ia.member " +
+            "LEFT JOIN FETCH ia.album " +
+            "LEFT JOIN FETCH ia.memory " +
+            "WHERE ia.id = :alarmId")
+    Optional<InviteAlarm> findByIdWithMemberAndAlbumAndMemory(@Param("alarmId") Long alarmId);
+
+    @Query(""" 
+            SELECT ia
+            FROM InviteAlarm ia
+            LEFT JOIN FETCH ia.memory m
+            LEFT JOIN FETCH ia.album al
+            WHERE ia.id = :alarmId
+            """)
+    Optional<InviteAlarm> findAlarmDetailsById(@Param("alarmId") Long alarmId);
 }
