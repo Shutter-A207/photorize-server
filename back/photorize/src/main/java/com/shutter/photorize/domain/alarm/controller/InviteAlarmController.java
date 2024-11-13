50746ab2ba281311464d72c46b699ddd5879f5fc
Lee9Bin <imagen33@daum.net>
2024-11-13 23:12:58 +0900

    Feat: 알람 상세 조회 API 구현
    InviteAlarmController
    알람 상세 조회 API 구현
    
    S11P31A207-403

--- back/photorize/src/main/java/com/shutter/photorize/domain/alarm/controller/InviteAlarmController.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/alarm/controller/InviteAlarmController.java
@@ -1,52 +1,52 @@
 package com.shutter.photorize.domain.alarm.controller;
 
-import static com.shutter.photorize.global.constant.CommonConstants.*;
-
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.shutter.photorize.domain.alarm.dto.request.InviteAlarmRequest;
+import com.shutter.photorize.domain.alarm.dto.response.InviteAlarmDetailResponse;
 import com.shutter.photorize.domain.alarm.dto.response.InviteAlarmResponse;
 import com.shutter.photorize.domain.alarm.service.InviteAlarmService;
 import com.shutter.photorize.global.jwt.model.ContextMember;
 import com.shutter.photorize.global.response.ApiResponse;
 import com.shutter.photorize.global.response.SliceResponse;
 import com.shutter.photorize.global.security.AuthUser;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static com.shutter.photorize.global.constant.CommonConstants.ALARM_PAGE_SIZE;
 
 @RestController
 @RequestMapping("/api/v1/alarms")
 @RequiredArgsConstructor
 public class InviteAlarmController {
 
-	private final InviteAlarmService inviteAlarmService;
+    private final InviteAlarmService inviteAlarmService;
 
-	@GetMapping
-	public ResponseEntity<ApiResponse<SliceResponse<InviteAlarmResponse>>> getInviteAlarms(
-		@AuthUser ContextMember contextMember,
-		@RequestParam(defaultValue = "0") int pageNumber) {
+    @GetMapping
+    public ResponseEntity<ApiResponse<SliceResponse<InviteAlarmResponse>>> getInviteAlarms(
+            @AuthUser ContextMember contextMember,
+            @RequestParam(defaultValue = "0") int pageNumber) {
 
-		Pageable pageable = PageRequest.of(pageNumber, ALARM_PAGE_SIZE);
+        Pageable pageable = PageRequest.of(pageNumber, ALARM_PAGE_SIZE);
 
-		return ApiResponse.ok(inviteAlarmService.getInviteAlarms(contextMember.getId(), pageable));
-	}
+        return ApiResponse.ok(inviteAlarmService.getInviteAlarms(contextMember.getId(), pageable));
+    }
 
-	@PostMapping("{alarmId}")
-	public ResponseEntity<ApiResponse<Void>> respondToInviteAlarm(
-		@PathVariable Long alarmId,
-		@RequestBody InviteAlarmRequest inviteAlarmRequest,
-		@AuthUser ContextMember contextMember) {
+    @PostMapping("{alarmId}")
+    public ResponseEntity<ApiResponse<Void>> respondToInviteAlarm(
+            @PathVariable Long alarmId,
+            @RequestBody InviteAlarmRequest inviteAlarmRequest,
+            @AuthUser ContextMember contextMember) {
 
-		inviteAlarmService.respondToInviteAlarm(contextMember.getId(), alarmId, inviteAlarmRequest);
-		return ApiResponse.ok(null);
-	}
+        inviteAlarmService.respondToInviteAlarm(contextMember.getId(), alarmId, inviteAlarmRequest);
+        return ApiResponse.ok(null);
+    }
+
+    @GetMapping("{alarmId}/detail")
+    public ResponseEntity<ApiResponse<InviteAlarmDetailResponse>> getInviteAlarmDetail(
+            @PathVariable Long alarmId,
+            @AuthUser ContextMember contextMember) {
+        return ApiResponse.ok(inviteAlarmService.getInviteAlarmDetail(contextMember.getId(), alarmId));
+    }
 }
