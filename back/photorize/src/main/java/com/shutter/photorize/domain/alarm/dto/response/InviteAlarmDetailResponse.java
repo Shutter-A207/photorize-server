9ab688c12653dbd94a4c2231b8a1b3ee5043ae24
Lee9Bin <imagen33@daum.net>
2024-11-14 11:29:07 +0900

    Feat: 알람 상세 조회 응답 필드 추가
    InviteAlarm
    알람 상세 조회 응답 필드 추가
    
    S11P31A207-403

--- back/photorize/src/main/java/com/shutter/photorize/domain/alarm/dto/response/InviteAlarmDetailResponse.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/alarm/dto/response/InviteAlarmDetailResponse.java
@@ -1,41 +1,48 @@
 package com.shutter.photorize.domain.alarm.dto.response;
 
+import java.util.List;
+
+import com.shutter.photorize.domain.alarm.entity.AlarmType;
 import com.shutter.photorize.domain.album.dto.response.AlbumMemberListResponse;
 import com.shutter.photorize.domain.album.entity.AlbumMemberList;
 import com.shutter.photorize.domain.file.entity.File;
 import com.shutter.photorize.domain.memory.entity.Memory;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class InviteAlarmDetailResponse {
 
-    private String url;
-    private String content;
-    private List<AlbumMemberListResponse> memberList;
+	private AlarmType alarmType;
+	private String url;
+	private String content;
+	private List<AlbumMemberListResponse> memberList;
 
-    @Builder
-    public InviteAlarmDetailResponse(String url, String content, List<AlbumMemberListResponse> memberList) {
-        this.url = url;
-        this.content = content;
-        this.memberList = memberList;
-    }
+	@Builder
+	public InviteAlarmDetailResponse(AlarmType alarmType, String url, String content,
+		List<AlbumMemberListResponse> memberList) {
+		this.alarmType = alarmType;
+		this.url = url;
+		this.content = content;
+		this.memberList = memberList;
+	}
 
-    public static InviteAlarmDetailResponse of(Memory memory, File file) {
-        return InviteAlarmDetailResponse.builder()
-                .content(memory.getContent())
-                .url(file.getUrl())
-                .build();
-    }
+	public static InviteAlarmDetailResponse of(AlarmType alarmType, Memory memory, File file) {
+		return InviteAlarmDetailResponse.builder()
+			.alarmType(alarmType)
+			.content(memory.getContent())
+			.url(file.getUrl())
+			.build();
+	}
 
-    public static InviteAlarmDetailResponse of(List<AlbumMemberList> albumMemberLists) {
-        return InviteAlarmDetailResponse.builder()
-                .memberList(albumMemberLists.stream().map(AlbumMemberListResponse::from).toList())
-                .build();
-    }
+	public static InviteAlarmDetailResponse of(AlarmType alarmType, List<AlbumMemberList> albumMemberLists) {
+		return InviteAlarmDetailResponse.builder()
+			.alarmType(alarmType)
+			.memberList(albumMemberLists.stream().map(AlbumMemberListResponse::from).toList())
+			.build();
+	}
 
 }
