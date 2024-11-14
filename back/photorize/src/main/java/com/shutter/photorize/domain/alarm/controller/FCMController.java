2080a1a0a2ff17f5e385f7163e920c3ed28319a3
Lee9Bin <imagen33@daum.net>
2024-11-14 11:16:41 +0900

    Feat: 회원 토큰 삭제 API 구현
    FCMController
    회원 토큰 삭제 API 구현
    
    S11P31A207-403

--- back/photorize/src/main/java/com/shutter/photorize/domain/alarm/controller/FCMController.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/alarm/controller/FCMController.java
@@ -2,11 +2,12 @@ package com.shutter.photorize.domain.alarm.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.shutter.photorize.domain.alarm.dto.request.FCMTokenSaveRequest;
+import com.shutter.photorize.domain.alarm.dto.request.FCMTokenRequest;
 import com.shutter.photorize.domain.alarm.service.FCMService;
 import com.shutter.photorize.global.jwt.model.ContextMember;
 import com.shutter.photorize.global.response.ApiResponse;
@@ -23,8 +24,16 @@ public class FCMController {
 
 	@PostMapping
 	public ResponseEntity<ApiResponse<Void>> saveToken(@AuthUser ContextMember contextMember,
-		@RequestBody FCMTokenSaveRequest fcmTokenSaveRequest) {
-		fcmService.saveToken(contextMember.getId(), fcmTokenSaveRequest);
+		@RequestBody FCMTokenRequest fcmTokenRequest) {
+		fcmService.saveToken(contextMember.getId(), fcmTokenRequest);
 		return ApiResponse.created();
+	}
+
+	@DeleteMapping
+	@PostMapping
+	public ResponseEntity<ApiResponse<Void>> deleteToken(@AuthUser ContextMember contextMember,
+		@RequestBody FCMTokenRequest fcmTokenRequest) {
+		fcmService.saveToken(contextMember.getId(), fcmTokenRequest);
+		return ApiResponse.ok(null);
 	}
 }
