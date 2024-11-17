3cbe3b1bff2bea2c99b29be758dd5041b7ac725a
Lee9Bin <imagen33@daum.net>
2024-11-17 20:05:53 +0900

    Feat: refreshUpdatedAt메서드 구현
    UpdatableEntity
    refreshUpdatedAt메서드 구현
    
    S11P31A207-411

--- back/photorize/src/main/java/com/shutter/photorize/global/entity/UpdatableEntity.java
+++ back/photorize/src/main/java/com/shutter/photorize/global/entity/UpdatableEntity.java
@@ -1,18 +1,21 @@
 package com.shutter.photorize.global.entity;
 
-import java.time.LocalDateTime;
-
-import org.springframework.data.annotation.LastModifiedDate;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
 public abstract class UpdatableEntity extends BaseEntity {
 
-	@LastModifiedDate
-	@Column(columnDefinition = "TIMESTAMP")
-	private LocalDateTime updatedAt;
+    @LastModifiedDate
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime updatedAt;
+
+    public void refreshUpdatedAt() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
