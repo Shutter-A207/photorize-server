007038a63a9b6224d55c9689fd076287ceca4535
최은혜 <eh.choi514@gmail.com>
2025-01-31 22:21:46 +0900

    Feat: TokenService에 토큰 재발급 로직 추가

--- back/photorize/src/main/java/com/shutter/photorize/global/jwt/service/TokenService.java
+++ back/photorize/src/main/java/com/shutter/photorize/global/jwt/service/TokenService.java
@@ -1,30 +1,66 @@
 package com.shutter.photorize.global.jwt.service;
 
-import java.util.concurrent.TimeUnit;
-
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.shutter.photorize.global.error.ErrorType;
+import com.shutter.photorize.global.exception.PhotorizeException;
 import com.shutter.photorize.global.jwt.model.TokenDto;
+import com.shutter.photorize.global.jwt.repository.RefreshTokenRepository;
 import com.shutter.photorize.global.jwt.util.JwtUtil;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class TokenService {
 	private final JwtUtil jwtUtil;
-	private final RedisTemplate<String, String> redisTemplate;
-	private static final String KEY_PREFIX = "refreshToken:";
+	private final RefreshTokenRepository refreshTokenRepository;
 
 	public TokenDto createToken(String userEmail) {
 
 		String accessToken = jwtUtil.createAccessToken(userEmail);
 		String refreshToken = jwtUtil.createRefreshToken(userEmail);
 
-		redisTemplate.opsForValue().set(KEY_PREFIX + userEmail, accessToken);
-		redisTemplate.expire(KEY_PREFIX + refreshToken, 14, TimeUnit.DAYS);
+		refreshTokenRepository.save(userEmail, refreshToken);
 
 		return TokenDto.of(accessToken, refreshToken);
+	}
+
+	@Transactional
+	public TokenDto reissueToken(String refreshToken, HttpServletResponse response) {
+		try {
+			validateRefreshToken(refreshToken);
+
+			String userEmail = jwtUtil.getEmail(refreshToken);
+			validateRedisRefreshToken(userEmail, refreshToken);
+
+			refreshTokenRepository.deleteByEmail(userEmail);
+
+			TokenDto newToken = createToken(userEmail);
+
+			response.addHeader("Authorization", "Bearer " + newToken.getAccessToken());
+			response.addHeader("Refresh-Token", newToken.getRefreshToken());
+
+			return newToken;
+		} catch (Exception e) {
+			throw new PhotorizeException(ErrorType.TOKEN_REISSUE_FAILED);
+		}
+	}
+
+	private void validateRefreshToken(String refreshToken) {
+		if (!jwtUtil.validation(refreshToken)) {
+			throw new PhotorizeException(ErrorType.EXPIRED_REFRESH_TOKEN);
+		}
+	}
+
+	private void validateRedisRefreshToken(String userEmail, String refreshToken) {
+		String redisRefreshToken = refreshTokenRepository.findByEmail(userEmail)
+			.orElseThrow(() -> new PhotorizeException(ErrorType.INVALID_REFRESH_TOKEN));
+
+		if (!redisRefreshToken.equals(refreshToken)) {
+			throw new PhotorizeException(ErrorType.INVALID_REFRESH_TOKEN);
+		}
 	}
 }
