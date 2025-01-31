63b034eec330e20f89cf0fa69247931753953ae0
최은혜 <eh.choi514@gmail.com>
2025-01-31 22:20:04 +0900

    Style: 주석 및 log 삭제

--- back/photorize/src/main/java/com/shutter/photorize/global/config/SecurityConfig.java
+++ back/photorize/src/main/java/com/shutter/photorize/global/config/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.shutter.photorize.domain.member.service.EmailService;
+import com.shutter.photorize.domain.member.service.AuthService;
 import com.shutter.photorize.domain.member.service.OauthService;
 import com.shutter.photorize.global.jwt.filter.JwtFilter;
 import com.shutter.photorize.global.jwt.filter.LoginFilter;
@@ -51,20 +51,15 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager,
 		JwtFilter jwtFilter,
-		EmailService emailService, OauthService oauthService) throws
+		AuthService authService, OauthService oauthService) throws
 		Exception {
 
 		//http basic 인증 방식 disable
 		http
 			.cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
-			//csrf disable
-			//세션방식에서는 세션이 항상 고정되기때문에 CSRF 공격을 필수적으로 방어해야함.
-			// JWT는 세션을 stateless 상태로 관리하기 때문에 CSRF에 대한 공격을 방어하지않아도되어서 disable로 둔다.
 			.csrf(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable)
-			//From 로그인 방식 disable
 			.formLogin(AbstractHttpConfigurer::disable)
-			// 경로별 인가 작업
 			.authorizeHttpRequests(authorize ->
 				authorize
 					.requestMatchers(
