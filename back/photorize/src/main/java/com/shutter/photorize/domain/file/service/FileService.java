7e99eb1d8f407bb601f186e0fe462a3ca7332f77
최은혜 <eh.choi514@gmail.com>
2025-03-04 15:39:23 +0900

    Feat: 해당 추억의 첫번째 사진 url 반환 메서드 생성

--- back/photorize/src/main/java/com/shutter/photorize/domain/file/service/FileService.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/file/service/FileService.java
@@ -45,6 +45,12 @@ public class FileService {
 			.toList();
 	}
 
+	public String getFileByMemory(Memory memory) {
+		return fileRepository.findFirstByMemoryAndTypeOrderByIdAsc(memory, FileType.PHOTO)
+			.map(file -> s3Utils.generatePreSignedUrl(file.getUrl()))
+			.orElse(null);
+	}
+
 	public void updateFile(List<MultipartFile> files, Memory memory) {
 		if (hasNewFiles(files)) {
 			files.forEach(file -> {
