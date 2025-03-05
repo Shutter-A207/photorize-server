eb46c424c309a2a2689f8d4c06b11983ccfcf8e8
최은혜 <eh.choi514@gmail.com>
2025-03-06 00:02:28 +0900

    Style: StringFormat으로 수정

--- back/photorize/src/main/java/com/shutter/photorize/global/constant/StringFormat.java
+++ back/photorize/src/main/java/com/shutter/photorize/global/constant/StringFormat.java
@@ -10,5 +10,7 @@ public class StringFormat {
 	public static final String PASSWORD_AUTH_PREFIX = "passwordAuth:";
 	public static final String PASSWORD_CHANGE_AVAILABLE_PREFIX = "validPasswordChange:";
 	public static final String REFRESH_TOKEN_PREFIX = "refreshToken:";
+	public static final String EMAIL_ADDRESS = "yangshutter@gmail.com";
+	public static final String EMAIL_PERSONAL = "Photorize";
 
 }
