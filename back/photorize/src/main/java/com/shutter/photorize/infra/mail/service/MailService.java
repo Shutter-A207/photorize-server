aad3e74d53b56659bdce93f7c54a03955efb212c
최은혜 <eh.choi514@gmail.com>
2025-03-06 00:03:39 +0900

    Feat: 이메일 인증코드용 쓰레드 설정

--- back/photorize/src/main/java/com/shutter/photorize/infra/mail/service/MailService.java
+++ back/photorize/src/main/java/com/shutter/photorize/infra/mail/service/MailService.java
@@ -6,6 +6,8 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.shutter.photorize.global.constant.StringFormat;
+
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -19,15 +21,17 @@ public class MailService {
 
 	private final JavaMailSender javaMailSender;
 
-	@Async
+	@Async("mailSendExecutor")
 	public void sendEmail(String to, String subject, String content, boolean isHtml) {
+		log.info("Executing sendEmail in thread: {}", Thread.currentThread().getName());
+
 		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 		try {
 			MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
 			mimeMessageHelper.setTo(to);
 			mimeMessageHelper.setSubject(subject);
 			mimeMessageHelper.setText(content, isHtml);
-			mimeMessageHelper.setFrom(new InternetAddress("yangshutter@gmail.com", "Photorize"));
+			mimeMessageHelper.setFrom(new InternetAddress(StringFormat.EMAIL_ADDRESS, StringFormat.EMAIL_PERSONAL));
 
 			javaMailSender.send(mimeMessage);
 
