2d9a8efe9e50842fa3f50b58b21af2e1f4c9f828
Lee9Bin <imagen33@daum.net>
2025-04-02 17:57:17 +0900

    Feat: 추억 응답 DTO에 memoryId, spotId 추가
    memoryId, spotId 필드 추가
    .of() 메서드에서 각각 memory.getId(), memory.getSpot().getId()로 값 설정

--- back/photorize/src/main/java/com/shutter/photorize/domain/memory/dto/response/MemoryDetailResponse.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/memory/dto/response/MemoryDetailResponse.java
@@ -14,33 +14,39 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MemoryDetailResponse {
 
+	private Long memoryId;
 	private Long writerId;
 	private String nickname;
 	private String writerImg;
 	private List<FileResponse> files;
 	private LocalDate date;
+	private Long spotId;
 	private String spotName;
 	private String content;
 
 	@Builder
-	private MemoryDetailResponse(Long writerId, String nickname, String writerImg, List<FileResponse> files, LocalDate date,
-		String spotName, String content) {
+	private MemoryDetailResponse(Long memoryId, Long writerId, String nickname, String writerImg,
+		List<FileResponse> files, LocalDate date, Long spotId, String spotName, String content) {
+		this.memoryId = memoryId;
 		this.writerId = writerId;
 		this.nickname = nickname;
 		this.writerImg = writerImg;
 		this.files = files;
 		this.date = date;
+		this.spotId = spotId;
 		this.spotName = spotName;
 		this.content = content;
 	}
 
 	public static MemoryDetailResponse of(Memory memory, List<FileResponse> files) {
 		return MemoryDetailResponse.builder()
+			.memoryId(memory.getId())
 			.writerId(memory.getMember().getId())
 			.nickname(memory.getMember().getNickname())
 			.writerImg(memory.getMember().getImg())
 			.files(files)
 			.date(LocalDate.from(memory.getDate()))
+			.spotId(memory.getSpot().getId())
 			.spotName(memory.getSpot().getName())
 			.content(memory.getContent())
 			.build();
