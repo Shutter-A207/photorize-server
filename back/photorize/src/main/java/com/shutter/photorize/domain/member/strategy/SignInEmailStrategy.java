28f28e3b81f0ececb461c9566b90a8e0fa9e5cf1
최은혜 <eh.choi514@gmail.com>
2025-04-11 18:18:37 +0900

    Feat : Redis 키 이름 및 유효시간 변경 (23시간 -> 5분)

--- back/photorize/src/main/java/com/shutter/photorize/domain/member/strategy/SignInEmailStrategy.java
+++ back/photorize/src/main/java/com/shutter/photorize/domain/member/strategy/SignInEmailStrategy.java
@@ -14,43 +14,46 @@ public class SignInEmailStrategy implements EmailCodeStrategy {
 	private final RedisAuthCodeAdapter redisAuthCodeAdapter;
 	private final MemberRepository memberRepository;
 
-	// 인증 코드를 5분동안 redis에 저장
+	// 인증 코드를 redis에 저장
 	@Override
 	public void save(String email, String code) {
 		if (memberRepository.existsByEmail(email)) {
 			throw new PhotorizeException(ErrorType.DUPLICATE_EMAIL);
 		}
 
-		redisAuthCodeAdapter.saveOrUpdate(generateSaveKey(email), code, 5);
+		// 이메일 인증 과정 중임을 저장(emailInProgress)
+		redisAuthCodeAdapter.saveOrUpdate(generateSaveProgressKey(email), String.valueOf(true), 10);
+		// 인증코드를 redis에 저장(emailCode)
+		redisAuthCodeAdapter.saveOrUpdate(generateSaveCodeKey(email), code, 300);
 	}
 
 	@Override
 	public boolean isProcessingEmail(String email) {
-		String key = generateSaveKey(email);
+		String key = generateSaveProgressKey(email);
 		return redisAuthCodeAdapter.hasKey(key) && redisAuthCodeAdapter.getExpireTime(key) > 0;
 	}
 
 	// 입력받은 인증코드의 유효성 검증
 	@Override
 	public boolean validAuthCode(String email, String code) {
-		String getCode = redisAuthCodeAdapter.getValue(generateSaveKey(email))
+		String getCode = redisAuthCodeAdapter.getValue(generateSaveCodeKey(email))
 			.orElseThrow(() -> new PhotorizeException(ErrorType.EXPIRED_EMAIL_CODE));
 		return getCode.equals(code);
 	}
 
-	private String generateSaveAuthKey(String email) {
-		return StringFormat.EMAIL_AUTH_PREFIX + email;
+	private String generateSaveCodeKey(String email) {
+		return StringFormat.EMAIL_CODE_PREFIX + email;
 	}
 
-	private String generateSaveKey(String email) {
-		return StringFormat.EMAIL_PROGRESS_PREFIX + email;
+	private String generateSaveProgressKey(String email) {
+		return StringFormat.EMAIL_IN_PROGRESS_PREFIX + email;
 	}
 
 	// 인증된 이메일을 유효한 상태로 표시
-	// Redis에 해당 이메일을 23시간동안 유효하다고 저장
+	// Redis에 해당 이메일을 5분동안 유효하다고 저장
 	@Override
 	public void pushAvailableEmail(String email) {
-		redisAuthCodeAdapter.saveOrUpdate(generateSaveAuthKey(email), String.valueOf(true), 1400);
+		redisAuthCodeAdapter.saveOrUpdate(generateAvailableKey(email), String.valueOf(true), 300);
 	}
 
 	// 이메일이 유효한 상태인지 확인
